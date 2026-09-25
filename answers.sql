9d927135b150dc997d2cb045e840c52905d0f60a
1U25CS032- Gunasekaran T <g8208003@gmail.com>
2026-09-25 12:46:36 +0530

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,1 +1,13 @@
+Assume 40 marks is the pass mark.
+SET SERVEROUTPUT ON;
+DECLARE
+    marks NUMBER := 65;
+BEGIN
+    IF marks >= 40 THEN
+        DBMS_OUTPUT.PUT_LINE('Student has Passed');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('Student has Failed');
+    END IF;
+END;
+/
 
